5aaaa8fca1c7890b43d86efe85c71905b6133c09
golubev <gs@lun.ua>
2020-07-25 14:16:24 +0300

    put the initial source code

---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const keys = require('./keys');
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000,
+});
+const sub = redisClient.duplicate();
+
+function calculateFibonacci(index) {
+    if (index < 2) {
+        return 1;
+    }
+    return calculateFibonacci(index - 1) + calculateFibonacci(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+    const messageAsInt = parseInt(message);
+    redisClient.hset(
+        'values',
+        message,
+        calculateFibonacci(messageAsInt)
+    );
+});
